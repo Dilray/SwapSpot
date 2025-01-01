19bd84f50f85de7ae716ef15ad70d3c2c78dc5be
Dilray <115149242+Dilray@users.noreply.github.com>
2025-01-01 23:17:17 +0300

    Full implementation of Spring Secuirty

---
+++ src/main/java/com/example/swapspot/configurations/SecurityConfig.java
@@ -0,0 +1,52 @@
+package com.example.swapspot.configurations;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final UserDetailsService userDetailsService;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/", "/product/", "/images/", "/registration").permitAll() // Разрешить доступ к указанным URL
+                        .anyRequest().authenticated() // Все остальные запросы требуют аутентификации
+                )
+                .formLogin(form -> form
+                        .loginPage("/login") // Указание страницы входа
+                        .permitAll() // Разрешить всем доступ к странице входа
+                )
+                .logout(logout -> logout
+                        .permitAll() // Разрешить всем доступ к выходу
+                );
+
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authManager(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder =
+                http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+        return authenticationManagerBuilder.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(8);
+    }
+}
